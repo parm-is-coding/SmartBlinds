435fe55d66582249bbb7696dc39ed0df3b7ada9e
parm-is-coding <pardhi1400@gmail.com>
2023-10-29 16:51:42 -0700

    add push button logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
-
+#include "A4988.h"
+// put variable declarations here:
+const int buttonPin = 2;
+int buttonState = LOW;
+A4988 stepper();
 // put function declarations here:
-int myFunction(int, int);
+int toggleBlinds(A4988 stepper);
 
-void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() {
+  pinMode(buttonPin,INPUT);
+
 }
 
-void loop() {
   // put your main code here, to run repeatedly:
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == HIGH){
+    toggleBlinds(stepper);
+    delay(10000);
+  }
+
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int toggleBlinds(A4988){
+  return 1;
 }
