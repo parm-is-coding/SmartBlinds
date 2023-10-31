5cf298c7f671c15b51ccb60c3e71669d07946c2e
parm-is-coding <pardhi1400@gmail.com>
2023-10-30 21:04:10 -0700

    refactor main logic into toggleBlinds

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ const int buttonPin = 2;
 int buttonState = LOW;
 A4988 stepper();
 // put function declarations here:
-int toggleBlinds(A4988 stepper);
+int toggleBlinds(A4988 stepper,int buttonState);
 
   // put your setup code here, to run once:
 void setup() {
@@ -16,14 +16,15 @@ void setup() {
   // put your main code here, to run repeatedly:
 void loop() {
   buttonState = digitalRead(buttonPin);
-  if(buttonState == HIGH){
-    toggleBlinds(stepper);
-    delay(10000);
-  }
+  //toggleBlinds(stepper, buttonState);
 
 }
 
 // put function definitions here:
-int toggleBlinds(A4988){
+int toggleBlinds(A4988 stepper,int buttonState){
+  if(buttonState == HIGH){
+    
+    delay(10000);
+  }
   return 1;
 }
