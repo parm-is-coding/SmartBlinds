971ca0c8698bedd00b536f5aaa73e0e2c33e78cc
parm-is-coding <pardhi1400@gmail.com>
2023-10-31 21:55:37 -0700

    implement toggleLED fn to test push buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,51 @@
 #include <Arduino.h>
 #include "A4988.h"
+#define buttonPin 4
+#define ledPin 13
 // put variable declarations here:
-const int buttonPin = 2;
 int buttonState = LOW;
-const int ledPin = 13;
+int ledState = LOW;
 //A4988 stepper();
-// put function declarations here:
-//int toggleBlinds(A4988 stepper,int buttonState);
 
-  // put your setup code here, to run once:
+// put function declarations here:
+
+//int toggleBlinds(A4988 stepper,int buttonState);
+//toggles the led on the board on or off based on the press of a push button
+void toggleLED(int buttonState, int& ledState);
+
 void setup() {
   pinMode(buttonPin,INPUT);
   pinMode(ledPin,OUTPUT);
 }
 
-  // put your main code here, to run repeatedly:
 void loop() {
-  digitalWrite(ledPin,LOW);
-  //buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(buttonPin);
+  toggleLED(buttonState,ledState);
+
   //digitalWrite(LED_BUILTIN,LOW);
   //toggleBlinds(stepper, buttonState);
 
 }
 
 // put function definitions here:
-int toggleBlinds(A4988 stepper,int buttonState){
+// int toggleBlinds(A4988 stepper,int buttonState){
+//   if(buttonState == HIGH){
+//     delay(10000);
+//   }
+//   return 1;
+// }
+
+
+void toggleLED(int buttonState,int& ledState){
   if(buttonState == HIGH){
-    
-    delay(10000);
+    if(ledState == HIGH){
+      ledState = LOW;
+      digitalWrite(ledPin,LOW);
+    }else{
+      ledState = HIGH;
+      digitalWrite(ledPin,HIGH);
+    }
+    delay(1000);
   }
-  return 1;
+  return;
 }
