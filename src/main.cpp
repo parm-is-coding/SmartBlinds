22c5a9c1a11b6445221401105e85dacd6d868005
parm-is-coding <pardhi1400@gmail.com>
2023-10-31 21:29:04 -0700

    change envoirment back to uno and tested flashing

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,22 @@
 // put variable declarations here:
 const int buttonPin = 2;
 int buttonState = LOW;
-A4988 stepper();
+const int ledPin = 13;
+//A4988 stepper();
 // put function declarations here:
-int toggleBlinds(A4988 stepper,int buttonState);
+//int toggleBlinds(A4988 stepper,int buttonState);
 
   // put your setup code here, to run once:
 void setup() {
   pinMode(buttonPin,INPUT);
-
+  pinMode(ledPin,OUTPUT);
 }
 
   // put your main code here, to run repeatedly:
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  digitalWrite(ledPin,LOW);
+  //buttonState = digitalRead(buttonPin);
+  //digitalWrite(LED_BUILTIN,LOW);
   //toggleBlinds(stepper, buttonState);
 
 }
