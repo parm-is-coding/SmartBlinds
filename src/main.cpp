c25dc7cf664795b9534fd8f553a5744c6478f554
parm-is-coding <pardhi1400@gmail.com>
2023-11-03 15:14:21 -0700

    removed stepper library since unneeded

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "A4988.h"
+
 //pin macros
 #define buttonPin 4
 #define ledPin 13
@@ -12,13 +12,13 @@ int ledState = LOW;
 // put function declarations here:
 
 //engage or disengage blinds based on buttonState
-void toggleBlinds(A4988& stepper,int& blindState);
+void toggleBlinds(int& blindState);
 //toggles the led on the board on or off based on the press of a push button
 void toggleLED(int& ledState);
 //spins the motor enough times that it sets the blinds in a extended position
-void engage(A4988& stepper);
+void engage();
 //spins the motor in the closing direction until blinds are closed;
-void disengage(A4988& stepper);
+void disengage();
 void rotateMotor360();
 
 void setup() {
@@ -53,13 +53,13 @@ void toggleLED(int& ledState){
   return;
 }
 //on click with toggle the blinds connected to the motor in the final state of the project
-void toggleBlinds(A4988& stepper,int& blindState){
+void toggleBlinds(int& blindState){
   if(blindState == HIGH){
     blindState =LOW;
-    disengage(stepper);
+    disengage();
   }else{
     blindState = HIGH;
-    engage(stepper);
+    engage();
   }
   delay(2000);
   return;
